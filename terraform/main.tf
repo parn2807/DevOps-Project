eb98483bf2ed8c49d265af2c36ac7d6387170d04
suphang <suphang40495@gmail.com>
2025-04-09 15:51:30 +0700

    change all terraform version

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,11 @@
 terraform {
-  required_version = ">= 1.0.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-
   backend "s3" {
-    key    = "aws/ec2-deploy/terraform.tfstate"
-
+    bucket = "parn-ex.project-bucket"
+    key    = "terraform/state.tfstate"
+    region = "ap-southeast-2"
   }
 }
+
 
 provider "aws"{
   region = var.region
