88761b52c6f46295c203c0bd516635737e72a484
suphang <suphang40495@gmail.com>
2025-04-09 13:56:05 +0700

    deploy infra

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    type        = string
+    default     = "us-east-1"
+    description = "description"
+}
+variable "public_key" {
+
+}
+variable "private_key" {
+
+}
+variable "key_name" {
+    
+}
